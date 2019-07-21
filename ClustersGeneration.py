cf303154d3c43cde20f081a6dcecf341258ddac8
lagripe <37987599+lagripe@users.noreply.github.com>
2019-07-21 16:10:52 +0100

    Update ClustersGeneration.py

--- ClustersGeneration.py
+++ ClustersGeneration.py
@@ -5,37 +5,29 @@ import re
 import numpy
 from py4j.java_gateway import JavaGateway
 
-
 coef = 0.2
 def ClusteringData(dir,coef=0.2):
     directory = listdir(dir)
-    SimilarityMatrix = {}
+    lengthDir = len(directory)
+    SimilarityMatrix = numpy.zeros((lengthDir,lengthDir),dtype=float)
 
-    for i in range(0,len(directory)):
+    for i,_WS in enumerate(directory):
 
-        _WS = directory[i]
         WS1 = PreprocessWSDL(dir+'\\'+_WS)
+        for j  in range(i,lengthDir):
 
-        if len(WS1['Name']) == 0:continue
-        SimilarityMatrix[_WS] = []
-        for j in range(0,len(directory)):
-
-            if i > j:continue
+            WS_ = directory[j]
             if i == j : 
-                SimilarityMatrix[_WS].append(0)
+                SimilarityMatrix [i,j]  = 0
                 continue
 
             WS_ = directory[j]
             WS2 = PreprocessWSDL(dir+'\\'+WS_)
-            
-            if len(WS2['Name']) == 0:continue
-
             # Similarity
-            
             SIM = SimilarityWSDLs(WS1,WS2,coef)
-            SimilarityMatrix[_WS].append(SIM)
+            SimilarityMatrix [i,j]  = SIM
+            SimilarityMatrix [j,i]  = SIM
             print(SIM)
-            
             print('---------------------------------------------')
     print(SimilarityMatrix)
 
@@ -44,11 +36,15 @@ def ClusteringData(dir,coef=0.2):
     Obj = myClassEntry.Cluster()
 '''
 import os
+directory = listdir('clearedDatabase')
 for WS in directory:
-    try:WS1 = PreprocessWSDL('qws-wsdls\\'+WS)
+    try:WS1 = PreprocessWSDL('clearedDatabase\\'+WS)
     except:
         continue
-    with(open('qws-wsdls\\'+WS,'r')) as f:
-        with(open('clearedDatabase\\'+WS,'w')) as fnew:
+    if len(WS1['Name']) == 0:continue
+    with(open('clearedDatabase\\'+WS,'r')) as f:
+        with(open('cleared\\'+WS,'w')) as fnew:
             fnew.write(f.read())
+
 '''
+
