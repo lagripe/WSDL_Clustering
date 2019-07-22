4958c8b6d864797ec4b19735a30b992c8c6d42a8
lagripe <37987599+lagripe@users.noreply.github.com>
2019-07-22 13:51:00 +0100

    fix

--- QTCluster/src/qtcluster/QTCluster.java
+++ QTCluster/src/qtcluster/QTCluster.java
@@ -5,14 +5,46 @@
  */
 package qtcluster;
 import py4j.GatewayServer;
+import edu.cornell.med.icb.clustering.MaxLinkageDistanceCalculator;
+import edu.cornell.med.icb.clustering.QTClusterer;
+import edu.cornell.med.icb.clustering.SimilarityDistanceCalculator;
+import java.util.ArrayList;
+import java.util.List;
 /**
  *
  * @author lagripe
  */
 public class QTCluster {
-    public int addition(float[][] matrix) {
-        return matrix.length;
-      }
+    public static int qtCluster(float[][] matrix) {
+        ArrayList<Float> list = new ArrayList<Float>();
+        for (int i = 0; i < matrix.length; i++)
+            for (int j = i + 1; j < matrix.length; j++)
+                list.add(matrix[i][j]);
+        final SimilarityDistanceCalculator distanceCalculator =
+                new MaxLinkageDistanceCalculator() {
+                    public double distance(final int i, final int j) {
+                        return Double.parseDouble(""+list.get(i)) ;
+                    }
+                };
+        int clusterSize = matrix.length * matrix[0].length;
+        System.out.println(" clusterSize " + list.size() );
+        final QTClusterer clusterer = new QTClusterer(list.size());
+        final List<int[]> clusters = clusterer.cluster(distanceCalculator, (float) 0.05);
+        
+        int[] t ;
+        for (int i = 0; i < clusters.size(); i++) {
+                t = clusters.get(i);
+                System.out.print(" { ");
+
+                for (int j = 0; j < t.length; j++) {
+                    System.out.print(t[j] + " ,");
+                }    
+                System.out.print(" } ");
+                System.out.println();
+        }
+        
+        return 0;
+    }
     public static int ParseMatrix(byte[] data) {
       java.nio.ByteBuffer buf = java.nio.ByteBuffer.wrap(data);
       int n = buf.getInt(), m = buf.getInt();
@@ -25,7 +57,7 @@ public class QTCluster {
               System.out.print(matrix[i][j]+" ");
           System.out.println("");
       }
-      return matrix.length;
+      return qtCluster(matrix);
    }
     public static void main(String[] args) {
         QTCluster app = new QTCluster();
